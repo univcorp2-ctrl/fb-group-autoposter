0cbbd492c05323487814f854462fbc7721e4d6b9
univcorp2-ctrl <univcorp2@gmail.com>
2026-06-11 18:15:24 +0900

    Build Facebook group autoposter pipeline

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "approval",
+    "generator",
+    "healer",
+    "ingest",
+    "logging_setup",
+    "orchestrator",
+    "poster",
+    "queue_db",
+    "selectors",
+    "session",
+    "verifier",
+]
